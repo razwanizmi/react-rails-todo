938786017d8939edef0b8ad3fcb042d7fb56b238
Razwan Rashidi <razwan.izmi@gmail.com>
2020-12-20 11:30:12 +0800

    Wire up basic Todo component

---
+++ app/javascript/components/Todos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import * as api from '../api'
+
+const Todos = () => (
+  <div>
+    <h1>Hello from Todo</h1>
+  </div>
+)
+
+export default Todos
---
+++ app/javascript/packs/todos_pack.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Todos from '../components/Todos'
+
+document.addEventListener('turbolinks:load', () => {
+  const targetNode = document.getElementById('react-app')
+  if (targetNode) {
+    ReactDOM.render(<Todos />, targetNode)
+  }
+})
